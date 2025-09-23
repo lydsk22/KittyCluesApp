9dc3ccdc0a47d4ed8f13b0f3e1263843f7060252
lydia <lydia.kidwell22@gmail.com>
2025-09-23 15:41:52 -0400

    test

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,11 +1,9 @@
-import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>Go to About Screen</Link>
+      <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
@@ -20,7 +18,7 @@ const styles = StyleSheet.create({
   text: {
     color: '#fff',
   },
-  button: {
+    button: {
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
