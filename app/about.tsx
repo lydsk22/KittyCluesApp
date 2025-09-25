0f0defd8bf479192855b67e3367f1cd5a2084b52
Veronica Kidwell <veronicak85@gmail.com>
2025-09-25 14:38:01 -0400

    Added Checkboxes

--- app/about.tsx
+++ app/about.tsx
@@ -1,13 +1,29 @@
+import { Checkbox } from 'expo-checkbox';
 import { Link } from 'expo-router';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from "react-native";
 
 export default function AboutScreen() {
+  const [isCheckedText, setCheckedText] = useState(false);
+  const [isCheckedEmail, setCheckedEmail] = useState(false);
+  const [isCheckedNotif, setCheckedNotif] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>How would you like to receive reports?</Text>
-      <Link href="/ages" style={styles.button}>Text</Link>
-      <Link href="/ages" style={styles.button}>Email</Link>
-      <Link href="/ages" style={styles.button}>Push Notification</Link>
+      <Text style={styles.header}>How would you like to receive reports? (Check all that apply)</Text>
+      <View style={styles.section}>
+        <Checkbox style={styles.checkbox} value={isCheckedText} onValueChange={setCheckedText} />
+        <Text style={styles.text}>Text</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox style={styles.checkbox} value={isCheckedEmail} onValueChange={setCheckedEmail} />
+        <Text style={styles.text}>Email</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox style={styles.checkbox} value={isCheckedNotif} onValueChange={setCheckedNotif} />
+        <Text style={styles.text}>Push Notification</Text>
+      </View>
+      <Link href="/ages" style={styles.button}>Done</Link>
     </View>
   );
 }
@@ -19,14 +35,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: 50,
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 50, color: '#fff',
+  },
+  subheader: {
+    fontSize: 20,
     color: '#fff',
   },
-    button: {
+  text: {
+    fontSize: 30,
+    color: '#fff',
+  },
+  button: {
     fontSize: 20,
     textDecorationLine: 'underline',
     color: '#fff',
+  },
+  checkbox: {
+    margin: 20,
   },
 
 });
